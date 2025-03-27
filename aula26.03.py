a0d668b5c1ca04a59989aa072a62156482d518fd
carloshmenon <hagamenon.gomes@gmail.com>
2025-03-26 21:49:44 -0400

    Update aula26.03.py

--- aula26.03.py
+++ aula26.03.py
@@ -13,3 +13,39 @@ print("\n \n")
 
 a=[1,2,1,5,6,4,3,1]
 print("o numero 1 aparece", a.count(1)," vezes. ")
+print("\n \n")
+
+#exer 3 subs o segundo elemento do vetor por laranja
+
+a=["maça","banana","pera","uva","beterraba"]
+a[1]=("laranja")
+print(a)
+print("\n \n")
+
+#exer 4 somando todas as listas
+
+t=[[1,2], [3], [4,5,6]]
+print(sum(t[0])+sum(t[1])+sum(t[2]))
+print("\n\n")
+
+#exer5 somando os elementos. 0 é ele mesmo, 1 é 1 mais 0 e 3 é 3+2+1
+
+t=[[1],[2],[3]]
+print (t[0])
+print (sum (t[0])+sum (t[1]))
+print (sum ((t)[0])+sum((t)[1])+sum((t)[2]))
+print ("\n \n")
+
+#exer 6, retirando os itens da lista
+
+t=[1,2,3,4,5]
+print (t[1:3])
+print("\n \n")
+
+#exerci 7 adicione o brasil na lista
+
+paises= ['alemanha', 'italia', 'japao']
+paises.extend(["Argentina", "Bolívia", "Brasil", "Chile", "Colômbia", "Equador", "Guiana", "Paraguai", "Peru", "Suriname", "Uruguai", "Venezuela" ])
+print("\n")
+paises.extend(["paises da america do norte","Canadá", "Estados Unidos", "México"])
+print (paises)
