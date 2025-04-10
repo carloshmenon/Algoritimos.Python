cc8ef7418d7404020310807d80603320758f1239
carloshmenon <hagamenon.gomes@gmail.com>
2025-04-10 18:53:02 -0400

    Update medi.salario.py

--- medi.salario.py
+++ medi.salario.py
@@ -8,14 +8,14 @@ if salario==500:
     aumento= salario*15 /100
     print(("seu novo salario é de, ",  salario))
 
-elif salario >=500:
+if salario >=500<1000:
     salario= salario*10 /100+salario
 
     aumento= salario*10 /100
     print(("seu novo salario é de, ",  salario))
 
-    #elif salario >1000:
-         #salario= salario*0.5 /100+salario
+elif salario>1000:
+         salario= salario*0.5 /100+salario
 
-    aumento= salario*0.5 /100
-    print(("seu novo salario é de, ",  salario))
+         aumento= salario*0.5 /100
+         print(("seu novo salario é de, ",  salario))
