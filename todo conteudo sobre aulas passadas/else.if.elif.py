f890db53bb5ea22cb234e844b890b6438a901a01
carloshmenon <hagamenon.gomes@gmail.com>
2025-05-05 20:53:15 -0400

    remove pastas

--- todo conteudo sobre aulas passadas/else.if.elif.py
+++
@@ -1,7 +0,0 @@
-idade=int(input("digite sua idade"))
-if idade>=16 and idade<18: ##caso verdadeiro
-    print("voce ja pode dirigir")
-elif idade <16:
-    print("apenas estude") 
-else: 
-    print("pode dirigir")       
