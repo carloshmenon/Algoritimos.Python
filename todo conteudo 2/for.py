507a087fb8c1d7293b4f76899c610d988e5c1537
carloshmenon <hagamenon.gomes@gmail.com>
2025-05-25 15:08:53 -0300

    todo conteudo 2

---
+++ todo conteudo 2/for.py
@@ -0,0 +1,5 @@
+#pega elemento por elemento na lista com o  for
+frutas = ["maça","banana","laranja"]
+for i in frutas:
+    print(i)
+    print("fim")
---
+++ todo conteudo 2/if.py
@@ -0,0 +1,8 @@
+# exemplo de condicional if else
+idade = int(input("Digite sua idade: "))
+if idade < 20:
+        print("Voce é jovem!")
+else:
+        print("Voce não é jovem")        
+
+        
