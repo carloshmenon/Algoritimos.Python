498103fbc364bd916bfdb5906b44e39a60228784
carloshmenon <hagamenon.gomes@gmail.com>
2025-07-21 20:48:10 -0400

    aula 21/07

---
+++ java.THIAGO/input.em.java
@@ -0,0 +1,19 @@
+// scanner é semelhante ao imput 
+// sc é a variavel e Scanner é o imput de dados 
+//no caso usando o nextInt para imput de dados na variavel sc trasformando a sc em number e dando o print dps 
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("digite um numero: ");
+	    int number =sc.nextInt();
+	    System.out.println("o numero digitado foi : " +number );
+	    
+	    
+
+	}
+}
