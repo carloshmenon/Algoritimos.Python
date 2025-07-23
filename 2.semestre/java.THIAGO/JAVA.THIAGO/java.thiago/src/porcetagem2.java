fd77b5ceb59aedd8f39645d951fef161ab9fbad5
carloshmenon <hagamenon.gomes@gmail.com>
2025-07-23 19:50:17 -0400

    aula

--- 2.semestre/java.THIAGO/JAVA.THIAGO/java.thiago/src/porcetagem2.java
+++ 2.semestre/java.THIAGO/JAVA.THIAGO/java.thiago/src/porcetagem2.java
@@ -3,7 +3,7 @@
 
 
 import java.util.Scanner;
-public class Main
+public class porcetagem2
 {
 	public static void main(String[] args) {
 	    
--- 2.semestre/java.THIAGO/JAVA.THIAGO/java.thiago/src/prints.java
+++ 2.semestre/java.THIAGO/JAVA.THIAGO/java.thiago/src/prints.java
@@ -5,7 +5,7 @@
 
 
 import java.util.Scanner;
-public class Main
+public class prints
 {
 	public static void main(String[] args) {
 	    
