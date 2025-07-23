ede73e7bfda85838d9a7ff8e9004099857b42fe4
carloshmenon <hagamenon.gomes@gmail.com>
2025-07-23 19:34:08 -0400

    AULA 23.07

--- 2.semestre/java.THIAGO/JAVA.THIAGO/Main.java
+++ 2.semestre/java.THIAGO/JAVA.THIAGO/Main.java
@@ -4,14 +4,20 @@
 
 
 import java.util.Scanner;
-public class Main
+public class Main 
+
+
+   
+
 {
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 	    
 	    Scanner sc = new Scanner(System.in);
 	    System.out.println("digite um numero: ");
 	    int number =sc.nextInt();
 	    System.out.println("o numero digitado foi : " +number );
+
+		sc.close();
 	    
 	    
 
