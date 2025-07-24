e75d71d83f581e9e2787440309884d97e94923e1
carloshmenon <hagamenon.gomes@gmail.com>
2025-07-23 20:13:02 -0400

    aula

---
+++ java.thiago/src/App2.java
@@ -0,0 +1,17 @@
+public class App2
+ {
+    public static void mani (String [] args ){
+        int x,y;
+        x=10;
+        y=20;
+        if(x > y){
+            System.out.println( "x é maior que Y");
+        }else{
+            System.out.println("y é maior que x");
+        }
+
+        
+    }
+}
+    
+
