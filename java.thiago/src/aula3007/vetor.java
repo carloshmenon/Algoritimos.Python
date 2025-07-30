9d83fd6f1bdede5372eef416095e511a8a27e2ae
carloshmenon <hagamenon.gomes@gmail.com>
2025-07-30 19:21:04 -0400

    aula30.07

---
+++ java.thiago/src/aula3007/vetor.java
@@ -0,0 +1,13 @@
+package aula3007;
+
+public class vetor {
+    public static void main(String[] args) {
+        
+        int[] vet= new int[10]; //tamanho do vetor ou listas como no .py
+        vet[0]=10; //marcamos o tamanho da variavel vet
+        vet[1]=vet [0]+ 10; // somamos a vet 0 +10 
+
+        System.out.println(vet[1]); //somamos a vet qque era 10 com 10, por isso o resultado sai 20
+
+    }
+}
---
+++ java.thiago/src/aula3007/vetor2.java
@@ -0,0 +1,14 @@
+package aula3007;
+
+public class vetor2 {
+
+    public static void main(String[] args) {
+        int num=10; //tamanho do vetor
+        int[] vet =new int[num];
+        int i;
+
+        for(i=0; i<=num; i++)
+        vet[i]=i;
+    }
+    
+}
