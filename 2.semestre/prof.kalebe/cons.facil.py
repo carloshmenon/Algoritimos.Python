a21b2b9827efb9093a227ed11fa55b85b599e3ef
carloshmenon <hagamenon.gomes@gmail.com>
2025-07-30 21:27:10 -0400

    aula

---
+++ 2.semestre/prof.kalebe/cons.facil.py
@@ -0,0 +1,9 @@
+##sistema construfacil
+
+print("bem vindo ao construfácil")
+
+funcionário = input(str("digite seu nome: \n"))
+input(str("digite sua senha : \n"))
+print("bem vindo ",funcionário)
+
+
