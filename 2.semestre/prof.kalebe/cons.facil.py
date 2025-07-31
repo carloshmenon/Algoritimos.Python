88a2f5bb0235f17c684053a07fc110bd00b34273
carloshmenon <hagamenon.gomes@gmail.com>
2025-07-30 21:33:51 -0400

    aula

--- 2.semestre/prof.kalebe/cons.facil.py
+++ 2.semestre/prof.kalebe/cons.facil.py
@@ -1,4 +1,6 @@
 ##sistema construfacil
+nova_venda=1
+estoque=2
 
 print("bem vindo ao construfácil")
 
@@ -6,4 +8,4 @@ funcionário = input(str("digite seu nome: \n"))
 input(str("digite sua senha : \n"))
 print("bem vindo ",funcionário)
 
-
+print("digite a opcao desejada ")
