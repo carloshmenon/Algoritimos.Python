fdd150a9bdd8ad7657836d93db4bf6b64d849394
carloshmenon <hagamenon.gomes@gmail.com>
2025-08-01 19:27:14 -0400

    rggfgfg

--- java.thiago/src/atv_1/exerc1.java
+++
@@ -1,5 +0,0 @@
-// 1. Crie um programa que leia 2 números inteiros e 1 real. Calcule e mostre: o produto do primeiro
-// com a metade do segundo, a soma do triplo do primeiro com o terceiro. O terceiro número
-// digitado ao cubo
-
-
