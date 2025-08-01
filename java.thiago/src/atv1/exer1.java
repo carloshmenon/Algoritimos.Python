11035a2307f44910888fd77726cd2c5fab0f6368
carloshmenon <hagamenon.gomes@gmail.com>
2025-08-01 19:02:38 -0400

    atv1

---
+++ java.thiago/src/atv1/exer1.java
@@ -0,0 +1,13 @@
+// 1. Crie um programa que leia 2 números inteiros e 1 real. Calcule e mostre: o produto do primeiro
+// com a metade do segundo, a soma do triplo do primeiro com o terceiro. O terceiro número
+// digitado ao cubo
+
+
+package atv1;
+public class exer1 {
+    
+    public static void main(String[] args) {
+        
+
+    }
+}
