02c55a18ed0dccbf9038d40e006c7c9002497372
carloshmenon <hagamenon.gomes@gmail.com>
2025-08-04 20:13:50 -0400

    java15

--- java.thiago/src/exer15.java
+++ java.thiago/src/exer15.java
@@ -1,3 +1,9 @@
+// Em uma empresa paga-se R$ 40,50 a hora e recolhe-se para o imposto de renda 11% dos salários
+// acima de R$ 2500,00. Dado o número de horas trabalhadas por um funcionário, informar o valor
+// do seu salário líquido.
+
+
+
 public class exer15 {
     public static void main(String[] args) {
         
