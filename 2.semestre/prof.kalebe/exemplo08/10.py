ded97d48268cc1419def44e3c75927696ee31eab
carloshmenon <hagamenon.gomes@gmail.com>
2025-10-08 21:36:56 -0400

    aula08/10

---
+++ 2.semestre/prof.kalebe/exemplo08/10.py
@@ -0,0 +1,10 @@
+
+class Aluno:
+    def __init__(self,aluno,matricula):
+        self.aluno=aluno
+        self.matricula=matricula
+
+a1=Aluno("carlos","123")
+
+print(a1.aluno, a1.matricula)
+        
