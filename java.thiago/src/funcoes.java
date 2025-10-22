42b19ef7a66a46d9e0e55b36bb83ac6b39906110
carloshmenon <hagamenon.gomes@gmail.com>
2025-10-22 19:34:37 -0400

    java

---
+++ java.thiago/src/funcoes.java
@@ -0,0 +1,14 @@
+
+
+public class funcoes {
+    public static void main(String[] args) {
+        hello();
+
+
+    }
+
+    public static void hello () {
+        System.out.println("Helooooooo!!");
+    }
+
+}
