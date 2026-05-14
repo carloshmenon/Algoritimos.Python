ff4fb35398dfe6504982f50e7a7769e65f420578
carloshmenon <hagamenon.gomes@gmail.com>
2026-05-13 21:46:20 -0400

    aula13-05-js

---
+++ 3.semestre/ederson/java script/aula13-05/variaveis.js
@@ -0,0 +1,45 @@
+//string
+
+//let nome ="ederson";
+//console.log(typeof nome); // aqui eu estou pedindo para o programa me falar oq é a variavel nome 
+
+//number num int inteiros ou decimais
+
+let idade =25;
+let altura=1.75;
+
+//bigint aqui declaramos numeros gigantes
+
+let numeroGigante= 1216548456456545454615615145n;
+
+//boolean V ou F
+
+let aprovado=true;
+let ligado= false;
+
+//undefined variavel sem valor
+
+let endereco;
+
+//null ausencia intencional de valor
+
+let telefone= null;
+
+//simbol gera um codigo, identificador unico
+
+let id = Symbol("id");
+
+//object conjunto de propriedades, entende que é onjeto quando abre uma chave
+
+let pessoa ={
+    nome:"carlos",
+    idade:30
+}
+
+//concatenar 
+//"meu nome é" +nome; 
+
+// armazenar dados de um prompt
+
+let nome = prompt("qual seu nome?");// aqui eu coloco um input
+alert("ola "+ nome+" seja bem vindo");// aqu eu junto tudo
