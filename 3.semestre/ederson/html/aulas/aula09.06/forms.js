ac6ea8e417bd28b72710b629d435c7ddbce98517
carloshmenon <hagamenon.gomes@gmail.com>
2026-06-09 21:48:21 -0400

    aula09/06

---
+++ 3.semestre/ederson/html/aulas/aula09.06/forms.js
@@ -0,0 +1,20 @@
+// validando se tem 3 digitos
+const inputs= document.querySelectorAll(".required");
+// criando uma funcao para exibir o erro
+const spans = document.querySelectorAll(".span-required"); // criando a constante
+function nameValidate(){
+    if(inputs[0].value.length<3){
+        setError(0);}
+
+    else{
+        removeError(0);}
+    
+    function removeError(index){
+        spans[index].style.display="none";}
+
+}
+
+// para multiplos elementos com uma classe, podemos utilizar document.querySlecetorALL()
+// e document.getElementsByClassName(). para selecionar apenas um unico
+
+// criando uma funcao para exibir o erro
