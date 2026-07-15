148dd3d068efc9c54f20ec8b57f94bac1cd71e44
carloshmenon <hagamenon.gomes@gmail.com>
2026-07-15 19:46:04 -0400

    htdocs

---
+++ 4.semestre/thiago/aula15.07/teste1.php
@@ -0,0 +1,12 @@
+<?php
+
+    $nome="meu nome é: ";
+    $meunome="thiago";
+
+    echo $nome.$meunome;
+
+    
+
+
+
+?>
---
+++ 4.semestre/thiago/aula15.07/teste2.php
@@ -0,0 +1,16 @@
+<?php
+
+    $x=10;
+    $y=20;
+    $res= $x*$y;
+
+    echo $res /2;
+
+    $res2 = ($x * $y)+$z;
+
+    echo "<br>".$res2;
+
+
+
+
+?>
