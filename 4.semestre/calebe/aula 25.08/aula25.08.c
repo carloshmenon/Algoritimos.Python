4c82a4a35a93546c1a1b2acd9148415e623d624b
carloshmenon <hagamenon.gomes@gmail.com>
2026-08-25 19:41:15 -0400

    atv calebe

---
+++ 4.semestre/calebe/aula 25.08/aula25.08.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+v (int numeros[]){
+  for (int i =0; i<5; i++) {
+    if (numeros[i]== 2) { 
+      printf ("encontrou!!");
+      }
+    }
+  }
+  
+int  main() {
+  int numeros[]={1,2,3,4,5];
+  buscar(numeros);
+  
+  return 0;
+}
